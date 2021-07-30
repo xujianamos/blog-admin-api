5520f31d09a9d19624323e0609bde79dfa024acc
xujian <2513573453@qq.com>
2021-07-30 23:16:27 +0800

    feat:新增分类信息查询接口

--- controllers/cate.js
+++ controllers/cate.js
@@ -110,7 +110,53 @@ function list(req, res) {
 }
 
 // 获取单条分类信息
-function info(req, res) {}
+function info(req, res) {
+  //  定义一个返回对象
+  const resObj = Common.clone(Constant.DEFAULT_SUCCESS)
+  // 定义一个async任务
+  let tasks = {
+    //  校验参数方法
+    checkParams: cb => {
+      // 调用公共方法中的校验参数方法，如果成功则继续后面的操作
+      // 如果失败则传递错误信息到async的最终方法中
+      Common.checkParams(req.params, ['id'], cb)
+    },
+    //  查询方法，依赖校验参数方法
+    query: [
+      'checkParams',
+      (results, cb) => {
+        //  使用cate的model中的方法查询
+        CateModel.findByPk(req.params.id)
+          .then(function (result) {
+            //  查询结果处理
+            //  如果查询到结果
+            if (result) {
+              //  将查询到的结果给返回对象赋值
+              resObj.data = {
+                id: result.id,
+                name: result.name,
+                createdAt: dateFormat(result.createdAt, 'yyyy-mm-dd HH:MM:ss')
+              }
+              // 继续后续操作
+              cb(null)
+            } else {
+              // 查询失败，传递错误信息到async最终方法中
+              cb(Constant.CATE_NOT_EXSIT)
+            }
+          })
+          .catch(function (err) {
+            // 错误处理
+            // 打印错误日志
+            console.log(err)
+            // 传递错误信息到async最终方法中
+            cb(Constant.DEFAULT_ERROR)
+          })
+      }
+    ]
+  }
+  // 执行公共方法中的autoFn方法，返回数据
+  Common.autoFn(tasks, res, resObj)
+}
 
 // 新增分类
 function add(req, res) {}
--- routes/cate.js
+++ routes/cate.js
@@ -6,12 +6,12 @@ const CateController = require('../controllers/cate')
 // 分类列表
 router.get('/', CateController.list)
 // 单条分类路由
-// router.get('/:id', CateController.info)
+router.get('/:id', CateController.info)
 // 添加分类路由
-// router.post('/', CateController.add)
+router.post('/', CateController.add)
 // 修改分类路由
-// router.put('/', CateController.update)
+router.put('/', CateController.update)
 // 删除分类
-// router.delete('/', CateController.remove)
+router.delete('/', CateController.remove)
 
 module.exports = router
