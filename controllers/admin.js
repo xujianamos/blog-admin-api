a6efea63f3ef9ddbbb487e5e6376ad6ef5c0e79f
xujian <2513573453@qq.com>
2021-08-01 22:44:01 +0800

    feat:添加管理员模块的注释

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,1 +1,8 @@
-// 管理员管理模块处理方法
+/*
+ * @Author       : xujian
+ * @LastEditors  : xujian
+ * @Date         : 2021-07-22 22:14:29
+ * @LastEditTime : 2021-08-01 22:08:28
+ * @Description  : 管理员管理模块处理方法
+ * @FilePath     : \blog-admin-api\controllers\admin.js
+ */
