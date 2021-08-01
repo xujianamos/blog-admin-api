c5c4c78a8e077d32194ed8581b416c5e6eb1d9df
xujian <2513573453@qq.com>
2021-08-01 22:46:28 +0800

    feat:修改文章管理模块路由

--- routes/article.js
+++ routes/article.js
@@ -1,18 +1,26 @@
+/*
+ * @Author       : xujian
+ * @LastEditors  : xujian
+ * @Date         : 2021-07-22 22:07:23
+ * @LastEditTime : 2021-08-01 22:09:07
+ * @Description  : file content
+ * @FilePath     : \blog-admin-api\routes\article.js
+ */
 // 文章管理模块路由
 const express = require('express')
 const router = express.Router()
 
-// const ArticleController = require("../controllers/article");
+const ArticleController = require('../controllers/article')
 
 // 文章列表路由
-// router.get("/", ArticleController.list);
+router.get('/', ArticleController.list)
 // 文章详情路由
-// router.get("/:id", ArticleController.info);
+router.get('/:id', ArticleController.info)
 // 新增文章路由
-// router.post("/", ArticleController.add);
+router.post('/', ArticleController.add)
 // 修改文章路由
-// router.put("/", ArticleController.update);
+router.put('/', ArticleController.update)
 // 删除文章路由
-// router.delete("/", ArticleController.remove);
+router.delete('/', ArticleController.remove)
 
 module.exports = router
